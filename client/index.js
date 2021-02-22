2c3c521ecff338cc6064518101e7a31baf969892
ferclaverino <fernando.claverino@gmail.com>
2021-02-22 12:23:14 -0300

    set full height

--- client/index.js
+++ client/index.js
@@ -2,7 +2,11 @@ import "regenerator-runtime/runtime";
 import { RepatersMap } from "./map";
 import { RepeaterService } from "./repeater-service";
 
+const height = $(window).height();
+
 window.addEventListener("load", async (event) => {
+  $("#map").height(height);
+
   const map = new RepatersMap("map", "tooltip");
   const repeaterService = new RepeaterService();
   const repeaters = await repeaterService.getAll();
