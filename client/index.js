0ccf2b23dbdfd8846c2dc0755f317a27e2d5af68
ferclaverino <fernando.claverino@gmail.com>
2021-02-22 12:48:31 -0300

    ask geolocation

--- client/index.js
+++ client/index.js
@@ -8,11 +8,19 @@ console.log(
   "Querés colaborar? Bienvenide: https://github.com/ferclaverino/repeaters"
 );
 
-window.addEventListener("load", async (event) => {
-  $("#map").height(height);
-
+async function buildMap() {
   const map = new RepatersMap("map", "tooltip");
   const repeaterService = new RepeaterService();
   const repeaters = await repeaterService.getAll();
   map.addRepeaters(repeaters);
+}
+
+function geoError() {
+  alert("Por favor permita acceder a su ubicación para ver mejor el mapa");
+  buildMap();
+}
+
+window.addEventListener("load", async (event) => {
+  $("#map").height(height);
+  navigator.geolocation.getCurrentPosition(await buildMap, geoError);
 });
